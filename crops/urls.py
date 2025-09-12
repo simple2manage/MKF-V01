d0aea1411406ed37e56d75e23db445295f5ed600
sree <sreejayam16@gmail.com>
2025-09-12 15:10:57 +0530

    nudges

--- crops/urls.py
+++ crops/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('zone-crops/', ZoneCropAPIView.as_view(), name='zone-crops'),
     path('user-crop-plan/',UserCropPlanAPIView.as_view(),name='crop-plan'),
     path('cropplan-row-create/',CropPlanRowListCreateView.as_view(),name='crop-plan-row-create'),
-    path('cropplan-row-update/',CropPlanRowFlexibleUpdateView.as_view(),name='crop-plan-row-create'),
+    path('cropplan-row-update/',CropPlanRowFlexibleUpdateView.as_view(),name='crop-plan-row-update'),
     path('user-crop-plan/<int:pk>/', UserCropPlanDeleteAPIView.as_view(), name='cropplan-delete'),
     path('send-crop-action/', SendCropPlanActionView.as_view(), name='send-crop-action')
 
